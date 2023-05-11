48bad4f6f95415603197610d15f2597d03013e0e
MartinLoidolt <martin.loidolt04@gmail.com>
2023-05-11 21:04:54 +0200

    Update MMM-neoomAPI.js
    new

--- MMM-neoomAPI.js
+++ MMM-neoomAPI.js
@@ -1,4 +1,5 @@
-Module.register("MMM-JokeAPI", {
+
+Module.register("MMM-neoomAPI", {
     defaults: {
         apiKey: "",
         siteId: "",
@@ -10,12 +11,12 @@ Module.register("MMM-JokeAPI", {
         ]
     },
     stats: null,
-    errorText: null,
+    errorText: "HI",
     notificationReceived(notification, payload, sender) {
         if (notification === 'MODULE_DOM_CREATED') {
             this.getStats();
             setInterval(() => {
-                this.getStats()
+                this.getStats();
             }, this.config.fetchInterval);
         }
     },
@@ -26,7 +27,7 @@ Module.register("MMM-JokeAPI", {
             const error = document.createElement("h1");
             error.className = "bright medium light fadeInJoke";
             error.innerHTML = this.errorText;
-            wrapper.appendChild(error)
+            wrapper.appendChild(error);
 
             return wrapper;
         }
@@ -47,23 +48,14 @@ Module.register("MMM-JokeAPI", {
         headerText.innerHTML = `Power Production: ${this.stats.power_production.value} Wh`;
         wrapper.appendChild(statsText);
     },
-    getStats() {
-        fetch(`https://api.ntuity.io/v1/sites/${this.config.siteId}/energy-flow/latest`, {
-            headers: {
-                Authorization: `Bearer ${this.config.apiKey}`
-            }
-        }).then((response) => {
+    async getStats() {
 
-            if(response.status !== 200) {
-                this.errorText = "Something went wrong. Check your Internet, ApiKey and SiteId.";
-            } else {
-                this.errorText = null;
-            }
+        fetch(`https://api.ntuity.io/v1/sites/${this.config.siteId}/energy-flow/latest/`).then((response) => {
 
-            response.json().then((stats) => {
-                this.stats = stats;
-                this.updateDom();
-            });
+		this.errorText = 'afterfetch';
+		this.updateDom();
+
+            
         });
     }
 });
